706af4cafada469f907ea56eb0b794640f893275
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-12 01:03:40 +0530

    Update Buy and sell stock.java

--- Day 1/Buy and sell stock.java
+++ Day 1/Buy and sell stock.java
@@ -16,3 +16,22 @@ class Solution {
 // Time complexity => O(n^2)
 
 // Optimized Approach
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int profit = 0;
+        int current = prices[0];
+        for(int i=1;i<prices.length;i++){
+            if(current > prices[i]){
+                current = prices[i];
+            }
+            else{
+                profit = prices[i] - current;
+                max = Math.max(profit, max);
+            }
+        }
+        return max;
+    }
+}
+
+// Time complexity => O(n)
