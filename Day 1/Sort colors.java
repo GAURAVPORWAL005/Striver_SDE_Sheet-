aedb7d4cf13c9adeb080dce76967937cdc952b9c
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-11 18:19:52 +0530

    Update Sort colors.java

--- Day 1/Sort colors.java
+++ Day 1/Sort colors.java
@@ -16,3 +16,29 @@ class Solution {
 }
 
 // Time complexity => O(n^2)
+
+// Optimized Approach
+class Solution {
+    public void sortColors(int[] nums) {
+        int c = 0;
+        int temp =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == 0){
+                 temp = nums[i];
+                nums[i] = nums[c];
+                nums[c] = temp;
+                c++;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == 1){
+                temp = nums[i];
+                nums[i] = nums[c];
+                nums[c] = temp;
+                c++;
+            }
+        }
+    }
+}
+
+// Time complexity = > O(n)
