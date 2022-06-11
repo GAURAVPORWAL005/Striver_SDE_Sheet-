6bdf762b23cbce44e48eb7d9e923c0850dfbf1e3
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-11 14:42:37 +0530

    Update Maximum SubArray.java

--- Day 1/Maximum SubArray.java
+++ Day 1/Maximum SubArray.java
@@ -16,4 +16,21 @@ class Solution {
 }
 // TIme complexity => O(n^2)
 
+// Optimized Appraoch
+
+// Kadane Algorithm
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+        for(int i=1;i<nums.length;i++){
+            currentSum = Math.max(currentSum + nums[i], nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+        return maxSum;
+    }
+}
+// Time complexity => O(n)
+
 
