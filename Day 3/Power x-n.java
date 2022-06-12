fa768d629197f79a5f17cad1e7f9e3cf75d7c512
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-13 02:53:26 +0530

    Create Power x-n.java

---
+++ Day 3/Power x-n.java
@@ -0,0 +1,22 @@
+// Question => https://leetcode.com/problems/powx-n/submissions/
+class Solution {
+    public double myPow(double x, int n) {
+        double temp;
+        if(n == 0){
+            return 1;
+        }
+        temp  = myPow(x, n/2);
+        if(n % 2 == 0){
+            return temp * temp;
+        }
+        else{
+            if(n >0){
+                return x * temp * temp;
+            }
+            else{
+                return (temp * temp) / x;
+            }
+        }
+    }
+}
+// TIme complexity => O(n)
