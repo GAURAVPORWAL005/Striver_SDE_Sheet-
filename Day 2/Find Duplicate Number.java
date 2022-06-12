ab3d5e99c9468169d05fb9c848f0b88ea1926c71
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-12 19:20:47 +0530

    Create Find Duplicate Number.java

---
+++ Day 2/Find Duplicate Number.java
@@ -0,0 +1,17 @@
+// Question => https://leetcode.com/problems/find-the-duplicate-number/
+// Brute Force Appraoch
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i] == nums[i+1]){
+                ans = nums[i];
+          }
+        }
+        return ans;
+    }
+        
+}
+
+// Time complexity => O(nlogn)
