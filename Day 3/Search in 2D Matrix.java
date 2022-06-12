ca2890488b36540dd3900058e0454b96d38b79c5
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-13 01:28:01 +0530

    Update Search in 2D Matrix.java

--- Day 3/Search in 2D Matrix.java
+++ Day 3/Search in 2D Matrix.java
@@ -1,4 +1,6 @@
 // Question => https://leetcode.com/problems/search-a-2d-matrix/submissions/
+
+// Brute force Approach
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         boolean isFound = false;
@@ -13,3 +15,24 @@ class Solution {
     }
 }
 // Time complexity => O(n^2)
+
+// Optimized Approach
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0; int j = matrix[0].length-1;
+        while(i < matrix.length && j >= 0){
+            if(matrix[i][j] == target){
+                return true;
+            }
+            if(target < matrix[i][j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
+}
+
+// Time complexity => O(n)
