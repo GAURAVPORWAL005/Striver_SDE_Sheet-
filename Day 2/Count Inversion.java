0a11a27441c984b44d7239763fb9a08a0123233d
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-12 23:53:15 +0530

    Create Count Inversion.java

---
+++ Day 2/Count Inversion.java
@@ -0,0 +1,16 @@
+// Question => https://www.codingninjas.com/codestudio/problems/count-inversions_615?leftPanelTab=0
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static long getInversions(long arr[], int n) {
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i] > arr[j] && i < j)
+                    count++;
+            }
+        }
+                return count;
+    }
+}
+// Time complexity => O(n^2)
