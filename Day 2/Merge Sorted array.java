063a0a54734518e8a12cbfb373e32cfcadb04777
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-12 19:04:31 +0530

    Create Merge Sorted array.java

---
+++ Day 2/Merge Sorted array.java
@@ -0,0 +1,12 @@
+// Quesiton => https://leetcode.com/problems/merge-sorted-array/submissions/
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=0;i<nums2.length;i++){
+            nums1[m] = nums2[i];
+            m++;
+        }
+        Arrays.sort(nums1);
+    }
+}
+
+// Time complexity => O(n)
