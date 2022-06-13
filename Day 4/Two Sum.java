5219718091f589df2262b6b9a52e46cfe2b10af9
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-14 00:45:03 +0530

    Update Two Sum.java

--- Day 4/Two Sum.java
+++ Day 4/Two Sum.java
@@ -17,3 +17,24 @@ class Solution {
 }
 
 // Time complexity => O(n^2)
+
+// Optimized Approach
+// if hashmap contains target - nums[i] value than return it otherwise add the nums[i] to the haspmap along with the index.
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target - nums[i])){
+                ans[0] = map.get(target - nums[i]);
+                ans[1] = i;
+                return ans;
+            }
+            map.put(nums[i], i);
+        }
+        return ans;
+    }
+}
+
+// Time Complexity => O(n)
