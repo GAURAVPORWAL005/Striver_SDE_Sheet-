98aba2908fe2685ae6d3cb517b8ac9acbf227dde
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-13 11:59:26 +0530

    Update Majority Element.java

--- Day 3/Majority Element.java
+++ Day 3/Majority Element.java
@@ -1,4 +1,7 @@
+
 // Question => https://leetcode.com/problems/majority-element/submissions/
+
+// Brute fore Approach
 class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
@@ -7,3 +10,25 @@ class Solution {
 }
 
 // Time complexity => O(logn)
+
+// Optimized Approach
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1;
+        int element = nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(count == 0){
+                element = nums[i];
+            }
+            if(element == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+            return element;
+    }
+}
+
+// Time complexity => O(n)
