4bcce3116143c119be31ff0791cfc557ac31dcc4
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-14 00:32:32 +0530

    Create Two Sum.java

---
+++ Day 4/Two Sum.java
@@ -0,0 +1,19 @@
+// Question => https://leetcode.com/problems/two-sum/submissions/
+class Solution {
+    int[] result = new int[2];
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+        
+    }
+}
+
+// Time complexity => O(n^2)
