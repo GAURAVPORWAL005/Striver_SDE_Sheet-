273740bec66ab5151fffaa86c4d1945d2a50974b
Gaurav Porwal <53477729+GAURAVPORWAL005@users.noreply.github.com>
2022-06-24 15:11:47 +0530

    Create SubSets II .java

---
+++ Day 9/SubSets II .java
@@ -0,0 +1,20 @@
+// QUestion => https://leetcode.com/problems/subsets-ii/
+class Solution {
+    public void subSet(int i, int[] nums,List<List<Integer>> list, List<Integer> ds){
+        list.add(new ArrayList<>(ds));
+        for(int index = i; index < nums.length;index++){
+            if(index != i && nums[index] == nums[index-1])
+                continue;
+            ds.add(nums[index]);
+            subSet(index+1, nums, list,ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+    
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        subSet(0,nums,list,new ArrayList<Integer>());
+        return list;
+    }
+}
